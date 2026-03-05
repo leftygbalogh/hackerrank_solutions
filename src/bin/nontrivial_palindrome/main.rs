aa9b80658f65d29598371a941e734cf38edda3eb
lefty <geza.balogh@trayport.com>
2026-03-05 20:28:57 +0100

    can't get the unit test to run now in the pali folder

--- src/bin/nontrivial_palindrome/main.rs
+++ src/bin/nontrivial_palindrome/main.rs
@@ -1,4 +1,24 @@
-mod palindrome;
-fn main(){
-	palindrome::printer(43);
+//mod palindrome;
+
+use std::io::{self, BufRead};
+use hackerrank_solutions::palindrome;
+/*
+ * Complete the 'isAlphabeticPalindrome' function below.
+ *
+ * The function is expected to return a BOOLEAN.
+ * The function accepts STRING code as parameter.
+ */
+
+//cargo run --bin nontrivial_palindrome
+fn main() {
+	println!("\x1b[2J\x1b[H\x1b[3J");
+	println!("nontrivial palindrome");
+	let stdin = io::stdin();
+	let mut stdin_iterator = stdin.lock().lines();
+
+	let code = stdin_iterator.next().unwrap().unwrap();
+
+	let result = palindrome::isAlphabeticPalindrome(&code);
+
+	println!("{}", if result { 1 } else { 0 });
 }
--- src/bin/nontrivial_palindrome/palindrome.rs
+++ src/bin/nontrivial_palindrome/palindrome.rs
@@ -1,18 +1,39 @@
-use rand::prelude::ThreadRng;
-
-pub fn printer(input: i32) -> i32 {
-	println!("Nontirival palindrome No: {}", &input);
-	input%2
-}
 
 //cargo test --bin proj_a
+
+pub fn isAlphabeticPalindrome(code: &str) -> bool {
+	let code = code
+		.chars()
+		.filter(|&c| c.is_alphabetic())
+		.collect::<String>()
+		.to_ascii_lowercase();
+
+	let reverse = code
+		.chars()
+		.rev()
+		.collect::<String>();
+
+	&code == &reverse
+}
+
+
 #[cfg(test)]
-mod tests {
+mod tests	{
 	use super::*;
-	#[test]
-	fn t1(){
-		assert_eq!(printer(1),1);
-		assert_eq!(printer(2),0);
+#[test]
+
+
+	fn rotate_test() {
+		let data1 = [("aAaAaAaAaAaAaaaa",true),("aAaÀÁÂÃÄÅàáâãäåɑΑαаᎪＡａdef£$%%*&^&*fed/ａＡᎪаαΑɑåäãâáàÅÄÃÂÁÀaAa",true),("aAaÀÁÂÃÄÅàáâãäåɑΑαаᎪＡａdef£$%%*&^&*fed/ａＡᎪаαΑɑåäãâáàÅÄÃÂÁÀaAa",true),(":։܃܄∶꞉：：꞉∶܄܃։:",true),("BbßʙΒβВЬᏴᛒＢｂｂＢᛒᏴЬВβΒʙßbB",true),("CcϲϹСсᏟⅭⅽ𐐠𺀠ＣｃｃＣ�𺀠�ⅽⅭᏟсСϹϲcCCcϲϹСсᏟⅭⅽ���ＣｃｃＣ�𺀠�ⅭᏟсСϹϲcC",false),("DdĎďĐđԁժᎠḍⅮⅾＤｄｄＤⅾⅮḍᎠժԁđĐďĎdD",true),("EeÈÉÊËéêëĒēĔĕĖėĘĚěΕЕеᎬＥｅｅＥᎬеЕΕěĚĘėĖĕĔēĒëêéËÊÉÈeE",true),];
+		let data2 = [("abc*def<>?fed+cba",true),("3D565A79&9kgFX6x4£$%%*&^&*4x6XFgk9/97A565D3",true),("4F1ED380'zdSigBg9£$%%*&^&*9gBgiSdz,083DE1F4",true),("FE77904F,TDLWLVeV<>?VeVLWLDT!F40977EF",true),("EBC49008!uRZqv1gf<>?fg1vqZRu%80094CBE",true),("94D0C00C)0RNbOJly,./$ylJObNR0,C00C0D49",true),("5C196FA5'PPc3mXVM}{P@:?><,./#'!£$%^&*()VXm3cPP+5AF691C5",false),("260B6B54$HwC5YcSJ<>?JScY5CwH+45B6B062",true),];
+		let data3 = [("6xczvB4/\"8D942)7IGiRC9m6B48D942)7Iwty  GiRC9m249D84B6",false),("AE500zxvcF35/XSVGbFK6AE500F35/wyXSVGb FK653F005EA",false),("E83C7FE2'zxcv56tHeJu2E83C7FEeu2'56tHeJu22EF7C38E",false),("B55C5E6E(fS9nzxcvjHsJB55ljghC5E6E(fS9njHsJE6E5C55B",false),("2F8770D1!oxIhEjr6eryuzxcv2F8770D1!oxIhEjr61D0778F2",false),("B007C751*bL1qhjlkaOtdB007xzcvC751*bL1qaOtd157C700B",false),("957417ED+YFeyqJvikv95sdf7417EDxzcv+YFqJvikvDE714759",false),("45A8A7Aadsf9#92QVoc6u45A8A7A9#92QzxvcVoc6u9A7A8A54",false),];
+		for data in [data1,data2,data3]{
+			_data_runner(data);}
 	}
 
+	fn _data_runner(data: [(&str, bool); 8]) {
+		for d in data.iter() {
+			assert_eq!(crate::palindrome::isAlphabeticPalindrome(d.0), d.1);
+		}
+	}
 }
